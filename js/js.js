63b672659e2da5489bb04bc1f9d2df04721822b2
Sasha Lelyk <133365539+lelyyykk@users.noreply.github.com>
2023-07-20 17:16:49 +0300

    upload js

--- js/js.js
+++ js/js.js
@@ -1,198 +1,157 @@
-
 /*
-  ========================================
-  BURGER-MENU
-  ========================================
+    ========================================
+    slider
+    ========================================
+*/
+$(document).ready(function() {
+    $('.banner__slider .row, .share__slider, .features__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 800,
+        cssEase: 'linear'
+    });
+});
+/*
+    ========================================
+    gallery
+    ========================================
 */
 document.addEventListener("DOMContentLoaded", function() {
-    const burgerMenu = document.querySelector(".burger-menu");
-    const menu = document.querySelector(".menu");
+    const galleryBlocks = document.querySelectorAll(".gallery__block");
+
+    galleryBlocks.forEach(function(block) {
+        const galleryImage = block.querySelector(".photo");
+        const blockUnder = block.querySelector(".gallery__block-under");
+
+        block.addEventListener("mouseenter", function() {
+            galleryImage.style.transform = "translateY(-84px)";
+            blockUnder.style.backgroundColor = "#000000";
+        });
+
+        block.addEventListener("mouseleave", function() {
+            galleryImage.style.transform = "none";
+            blockUnder.style.backgroundColor = "rgba(0, 0, 0, 1)";
+        });
+    });
+});
+
+/*
+    ========================================
+    like
+    ========================================
+*/
+document.addEventListener("DOMContentLoaded", function() {
+    const likeButtons = document.querySelectorAll(".like-button");
+
+    likeButtons.forEach(function(likeButton) {
+        const filledHeart = likeButton.querySelector(".fas");
+        const emptyHeart = likeButton.querySelector(".far");
+        const likeCount = likeButton.querySelector(".like-count");
+        let isLiked = false;
+        let count = parseInt(likeCount.innerText);
+
+        likeButton.addEventListener("click", function() {
+            if (isLiked) {
+                filledHeart.style.display = "none";
+                emptyHeart.style.display = "inline-block";
+                count--;
+            } else {
+                filledHeart.style.display = "inline-block";
+                emptyHeart.style.display = "none";
+                count++;
+            }
+            likeCount.innerText = count;
+            isLiked = !isLiked;
+        });
+    });
+});
+/*
+    ========================================
+    Validation
+    ========================================
+*/
+$(document).ready(function() {
+    // Form validation
+    $(".form-inline").submit(function(event) {
+        event.preventDefault();
+
+        var form = $(this);
+        var input = form.find("input[type='text']");
+        var isValid = true;
+        var errorContainer = input.siblings(".error-message");
+        var errorMessage = input.data("error");
+
+        if (input.val().trim() === "") {
+            input.addClass("error");
+            input.attr("placeholder", errorMessage);
+            isValid = false;
+        } else if (!/^.{15,}$/.test(input.val()) || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.val())) {
+            input.addClass("error");
+            input.val("");
+            input.attr("placeholder", "Enter the correct email");
+            isValid = false;
+        } else {
+            input.removeClass("error");
+        }
+
+        if (isValid) {
+            form[0].reset();
+            input.attr("placeholder", "Your email");
+        }
+    });
+});
+
+
+/*
+    ========================================
+    BURGER-MENU
+    ========================================
+*/
+document.addEventListener("DOMContentLoaded", function() {
+    const burgerMenu = document.querySelector(".header__burger-menu");
+    const menu = document.querySelector("ul");
 
     burgerMenu.addEventListener("click", function() {
-        menu.classList.toggle("show");
-        burgerMenu.classList.toggle("active"); // Додали цей рядок
+        menu.parentElement.classList.toggle("show");
+        burgerMenu.classList.toggle("active");
     });
 
     const menuLinks = menu.querySelectorAll("a");
     menuLinks.forEach(function(link) {
         link.addEventListener("click", function() {
-            menu.classList.remove("show");
-            burgerMenu.classList.remove("active"); // Додали цей рядок
+            menu.parentElement.classList.remove("show");
+            burgerMenu.classList.remove("active");
         });
     });
 });
 
-/*slider*/
-$(document).ready(function() {
-    $('.slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 10000,
-        cssEase: 'linear'
-    })
-})
-
 
 /*
-  ========================================
-  Hidden menu
-  ========================================
+    ========================================
+    for scroll
+    ========================================
 */
-// Отримуємо посилання на елементи списку та підменю
-const items = document.querySelectorAll('.items');
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[data-scroll]');
 
-// Проходимося по кожному елементу з класом .items
-items.forEach(function(item) {
-    const hiddenMenu = item.querySelector('.hidden-menu');
+    links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+    event.preventDefault();
 
-    // Додаємо події наведення курсора та відведення курсора для поточного елементу
-    item.addEventListener('mouseover', showHiddenMenu);
-    item.addEventListener('mouseout', hideHiddenMenu);
+        const target = document.querySelector(link.getAttribute("href"));
+        const offset = target.getBoundingClientRect().top + window.scrollY;
+        const windowHeight = window.innerHeight;
+        const scrollToPosition = offset - (windowHeight / 2) + (target.offsetHeight / 2);
 
-    // Функція для відображення прихованого підменю
-    function showHiddenMenu() {
-        hiddenMenu.classList.add('visible');
-    }
-
-    // Функція для приховування прихованого підменю
-    function hideHiddenMenu() {
-        hiddenMenu.classList.remove('visible');
-    }
-});
-
-
-
-
-/*
-  ========================================
-  For form
-  ========================================
-*/
-$(document).ready(function() {
-    // Show the form
-    $('.connection').click(function() {
-        $('.feedback-hidden-menu').addClass('visible');
-    });
-
-    // Close the form
-    $('.feedback-hidden-menu .submit').click(function() {
-        if (isFormValid()) {
-            $('.feedback-hidden-menu').removeClass('visible');
-        } else {
-            return false;
-        }
-    });
-
-    // Close the form when clicked outside
-    $(document).click(function(event) {
-        if (!$(event.target).closest('.connection').length && !$(event.target).closest('.feedback-hidden-menu').length) {
-            $('.feedback-hidden-menu').removeClass('visible');
-        }
-    });
-
-    // Close the form when close button is clicked
-    $('.feedback-hidden-menu .close-button').click(function() {
-        $('.feedback-hidden-menu').removeClass('visible');
-        return false;
-    });
-
-    // Form validation
-    function isFormValid() {
-        var inputs = $('.feedback-hidden-menu form input[type="text"], .feedback-hidden-menu form input[type="tel"], .feedback-hidden-menu form input[type="email"], .feedback-hidden-menu form textarea');
-        var isValid = true;
-
-        inputs.each(function() {
-            var input = $(this);
-
-            if (input.val().trim() === '') {
-                input.addClass('error');
-                isValid = false;
-            } else {
-                input.removeClass('error');
-            }
+                window.scroll({
+                top: scrollToPosition,
+                behavior: "smooth",
+            });
         });
-
-        return isValid;
-    }
-});
-
-
-/*
-  ========================================
-  Validation
-  ========================================
-*/
-
-$(document).ready(function() {
-    // Show the form
-    $('.connection').click(function() {
-        $('.feedback-hidden-menu').addClass('visible');
-    });
-
-    // Close the form when clicked outside
-    $(document).click(function(event) {
-        if (!$(event.target).closest('.connection').length && !$(event.target).closest('.feedback-hidden-menu').length) {
-            $('.feedback-hidden-menu').removeClass('visible');
-        }
-    });
-
-    // Close the form when close button is clicked
-    $('.feedback-hidden-menu .close-button').click(function() {
-        $('.feedback-hidden-menu').removeClass('visible');
-        return false;
-    });
-
-    // Form validation
-    $(".feedback-hidden-menu form").submit(function(event) {
-        event.preventDefault();
-
-        var form = $(this);
-        var inputs = form.find("input[type='text'], input[type='tel'], input[type='email'], textarea");
-        var isValid = true;
-
-        inputs.each(function() {
-            var input = $(this);
-            var errorContainer = input.siblings(".error-message");
-            var errorMessage = input.data("error");
-            var fieldType = input.attr("type");
-
-            if (input.val().trim() === "") {
-                input.addClass("error");
-                input.attr("placeholder", errorMessage);
-                isValid = false;
-            } else {
-                if (fieldType === "text" && !/^[a-zA-Zа-яА-ЯіїєІЇЄґҐ']{2,}$/.test(input.val())) {
-                    input.addClass("error");
-                    input.val("");
-                    input.attr("placeholder", "Введіть коректно ім'я.");
-                    isValid = false;
-                } else if (fieldType === "tel" && !/^[\d-()+ ]{10,}$/.test(input.val())) {
-                    input.addClass("error");
-                    input.val("");
-                    input.attr("placeholder", "Введіть коректний номер телефону.");
-                    isValid = false;
-                } else if (fieldType === "email" && (!/^.{15,}$/.test(input.val()) || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.val()))) {
-                    input.addClass("error");
-                    input.val("");
-                    input.attr("placeholder", "Введіть коректну адресу електронної пошти.");
-                    isValid = false;
-                } else if (input.is("textarea[name='message']") && input.val().length < 10) {
-                    input.addClass("error");
-                    input.val("поле обов'язкове");
-                    input.attr("placeholder", "Введіть повідомлення (мінімум 10 символів).");
-                    isValid = false;
-                } else {
-                    input.removeClass("error");
-                }
-            }
-        });
-
-        if (isValid) {
-            $('.feedback-hidden-menu').removeClass('visible');
-            form[0].reset();
-        }
     });
 });
+
+
